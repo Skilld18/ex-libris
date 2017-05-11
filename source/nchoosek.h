c88ec00b78e64380b466053b4fb55cf059e746d8
Russell Dunk <russelldunk@gmail.com>
2017-05-11 14:48:30 -0400

    Fixing some template stuff

--- source/nchoosek.h
+++ source/nchoosek.h
@@ -1,6 +1,29 @@
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
-vector<int> n_choose_k(vector<int> n, int k, bool (*valid)(vector<int> data));
-void test();
+template <typename T, typename A>
+vector<T,A> n_choose_k(vector<T,A> n, int k, bool (*valid)(vector<T,A> data)) {
+    typename vector<T,A>::iterator it = n.begin();
+    vector<T,A> result;
+    while (result.size() < k) {
+        if (it == n.end()) {
+	    int x = result[result.size()-1];
+	    it = find(n.begin(), n.end(), x);
+	    it++;
+            result.pop_back();
+        }
+        while (it != n.end()) {
+            result.push_back(*it);
+            if (valid(result)) {
+                ++it;
+                break;
+            }
+            result.pop_back();
+            it++;
+        }
+    }
+    return result;
+}
 
