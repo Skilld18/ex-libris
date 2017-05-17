8af50d0beb3c5ee33ec506bfd69dfb2656a9ea06
Russell Dunk <russelldunk@gmail.com>
2017-05-17 18:49:07 -0400

    Adding a sort of stack var

--- source/nchoosek.h
+++ source/nchoosek.h
@@ -2,14 +2,14 @@
 #include <iostream>
 #include <algorithm>
 //TODO:: check if set will never exist
+//TODO:: allow duplicates?
 using namespace std;
 template <typename T, typename A>
 vector<T,A> n_choose_k(vector<T,A> n, vector<typename vector<T,A>::iterator> stack, int k, bool (*valid)(vector<T,A> data)) {
     //TODO:: pass this in?
-    //typename vector<T,A>::iterator it = n.begin();
-    stack .push_back(n.begin());
+    stack.push_back(n.begin());
     vector<T,A> result;
-    //size of the current vector
+    //size of the current n vector
     while (result.size() < k) {
         //if we are at the end of the iterator pop the stack and increment the end of the stack
         if (stack.back() == n.end()) {
